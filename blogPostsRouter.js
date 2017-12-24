92602ef4e4e8caf14c9bc649f38cbfc7305c5374
Kyle Baker <kylekb1@gmail.com>
2017-12-24 13:47:25 -0500

    Added Mongoose, refactored CRUD operations

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -1,35 +1,47 @@
 const express = require('express');
 const router = express.Router();
-
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+const mongoose = require('mongoose');
 
-const {BlogPosts} = require('./models');
+// Mongoose internally uses a promise-like object,
+// but its better to make Mongoose use built in es6 promises
+mongoose.Promise = global.Promise;
 
-function lorem() {
-  return 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ' +
-    'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, '
-    'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ' +
-    'consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
-    'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ' +
-    'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-}
+const { Post } = require('./models');
 
-// seed some posts so initial GET requests will return something
-BlogPosts.create(
-  '10 things -- you won\'t believe #4', lorem(), 'Billy Bob');
-BlogPosts.create(
-  'Lions and tigers and bears oh my', lorem(), 'Lefty Lil');
 
 //Get all blog post
 router.get('/', (req, res) => {
-  res.json(BlogPosts.get());
+  Post
+  .find()
+  .limit(20)
+  .then(posts => {
+    res.json({
+      posts: posts.map(
+        (post) => post.serialize())
+    });
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  });
 });
 
+//Request post by ID
+router.get('/:id', (req, res) => {
+  Post
+    .findById(req.params.id)
+    .then(post => res.json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error'});
+    });
+});
 
 //Create a new blog post
 router.post('/', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
+
   const requiredFields = ['title', 'content', 'author'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -39,45 +51,55 @@ router.post('/', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-  const item = BlogPosts.create(
-    req.body.title, req.body.content, req.body.author);
-  res.status(201).json(item);
+
+  Post
+    .create({
+    title: req.body.title,
+    content:req.body.content,
+    author: {
+      firstName: req.body.author.firstName,
+      lastName: req.body.author.lastName
+    }
+    })
+    .then(post => res.status(201).json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
 });
+
 
 //Update blog post
 router.put('/:id', jsonParser, (req, res) => {
-  const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  if (req.params.id !== req.body.id) {
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message = (
-      `Request path id (${req.params.id}) and request body id `
+      `Request path id (${req.params.id}) and request body id ` +
       `(${req.body.id}) must match`);
     console.error(message);
-    return res.status(400).send(message);
+    return res.status(400).json({ message: message });
   }
-  console.log(`Updating blog post with id \`${req.params.id}\``);
-  const updatedItem = BlogPosts.update({
-    id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-    publishDate: req.body.publishDate
+
+  const toUpdate = {};
+  const updateableFields = ['title', 'content', 'author'];  
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
   });
-  res.status(204).end();
+
+  Post
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate})
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
 //Delete blog post
 router.delete('/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  console.log(`Deleted blog post with id \`${req.params.ID}\``);
-  res.status(204).end();
+  Post
+    .findByIdAndRemove(req.params.id)
+    .then(post => res.status(204).end())
+      .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
 
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
 const blogPostsRouter = require('./blogPostsRouter');
-const app = express();
 
+const { PORT, DATABASE_URL } = require('./config');
+mongoose.Promise = global.Promise;
+
+const app = express();
 
 app.use(morgan('common'));
 app.use('/blog-posts', blogPostsRouter);
 
+// catch-all endpoint if client makes request to non-existent endpoint
+app.use('*', function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
+});
 
 // both runServer and closeServer need to access the same
 // server object, so we declare `server` here, and then when
@@ -17,14 +24,20 @@ let server;
 // this function starts our server and returns a Promise.
 // In our test code, we need a way of asynchronously starting
 // our server, since we'll be dealing with promises there.
-function runServer() {
-  const port = process.env.PORT || 8080;
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+    mongoose.connect(databaseUrl, {useMongoClient: true}, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
     });
   });
 }
@@ -33,15 +46,15 @@ function runServer() {
 // `server.close` does not return a promise on its own, so we manually
 // create one.
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        // so we don't also call `resolve()`
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
   });
 }
